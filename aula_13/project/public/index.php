a71411c8fc5ad754fd6633e8db33d965bb29f3dd
Felipe Vieira <felipe@codeeasy.com.br>
2021-04-12 08:33:25 -0300

    Aula 13 - Adding JSON Middleware

--- aula_13/project/public/index.php
+++ aula_13/project/public/index.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 use Slim\Factory\AppFactory;
 use Slim\Psr7\Response as Psr7Response;
@@ -25,8 +26,6 @@ $myMiddleware = function (Request $request, RequestHandler $handler): Response {
     return $response;
 };
 
-$app->add($myMiddleware);
-
 class ExampleMiddleware
 {
     public function __invoke(Request $request, RequestHandler $handler): Response
@@ -42,8 +41,35 @@ class ExampleMiddleware
     }
 }
 
+class JsonBodyParserMiddleware implements MiddlewareInterface
+{
+    public function process(Request $request, RequestHandler $handler): Response
+    {
+        $contentType = $request->getHeaderLine('Content-Type');
+
+        if (strstr($contentType, 'application/json')) {
+            $contents = json_decode(file_get_contents('php://input'), true);
+
+            if (json_last_error() === JSON_ERROR_NONE) {
+                $request = $request->withParsedBody($contents);
+            }
+        }
+
+        return $handler->handle($request);
+    }
+}
+
+$app->add($myMiddleware)
+    ->add(new JsonBodyParserMiddleware());
+
 $app->get('/', function (Request $request, Response $response, array $args): Response {
     $response->getBody()->write("Hello world!");
+    return $response;
+})->add(new ExampleMiddleware());
+
+$app->post('/', function (Request $request, Response $response): Response {
+    $data = $request->getParsedBody();
+    $response->getBody()->write($data['name']);
     return $response;
 })->add(new ExampleMiddleware());
 
